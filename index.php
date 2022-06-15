817ce3e6b53154ddebeaef06201e975d66877b24
Elián Barrios <elianbarrios1707@outlook.com>
2022-06-14 21:23:49 -0600

    version 1.1.1

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php session_start();
 
 if (isset($_SESSION['usuario'])) {
-  header('Location: ../content.php');
+  header('Location: content.php');
 } else{
-  header('Location: ../sign-in.php');
+  header('Location: sign-in.php');
 }
 
- ?>
+?>
